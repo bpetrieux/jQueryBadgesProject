81a371e2e4a3518c4377beab21b6f19311f68b26
bpetrieux <benjaminpetrieux@gmail.com>
2017-04-27 12:53:11 -0400

    build done

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,18 @@
 $(function() {
 
-  // your code will go here
+   $.ajax({
+    url: 'https://www.codeschool.com/users/benjaminpetrieux.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+
+
+    $.each(response.courses.completed, function(course,element) {
+    	console.log(element.url);
+    	$('#badges').append('<div class="course"><h3>'+ element.title +'</h3><img src="'+ element.badge+'" alt="" /><a class="btn btn-primary" href="'+ element.url +'" target="_blank">See Course</a></div>');
+});
+
+    }
+  });
 
 });
